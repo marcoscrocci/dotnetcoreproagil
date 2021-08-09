de289de66917000d34607945a3ffb84767aac423
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-08-09 20:40:25 -0300

    Instalação do bootstrap e fontawesome. Obter um navbar do site do bootstrap.

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any; /* = [
+  eventos: any;
+  /** = [
     {
       EventoId: 1,
       Tema: 'Angular',
