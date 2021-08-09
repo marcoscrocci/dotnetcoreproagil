95b818c66b8ba00f92d72b6ef0b6d5cf1bb38e92
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-08-09 12:20:29 -0300

    Até aula 24 - Sem implementar o Retorno Assíncrono

--- ProAgil.API/Controllers/EventoController.cs
+++ ProAgil.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ProAgil.API.Data;
 using ProAgil.API.Model;
+using Microsoft.AspNetCore.Http;
 
 namespace ProAgil.API.Controllers
 {
@@ -19,29 +20,17 @@ namespace ProAgil.API.Controllers
 
         // GET api/evento
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        public IActionResult Get()
         {
-            return _context.Eventos.ToList();
-            /**
-            return new Evento[] {
-                new Evento {
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento {
-                    EventoId = 2,
-                    Tema = "Angular e Suas Novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            };
-            */
+            try
+            {
+                var results = _context.Eventos.ToList();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou!");
+            }
         }
 
         //GET api/evento/1
